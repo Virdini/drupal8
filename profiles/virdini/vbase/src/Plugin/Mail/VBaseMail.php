8444a234587a7506b2e87957f0ed8e010070540c
Alexander Tsybenko <alexander@tsybenko.info>
2017-02-07 20:03:27 +0200

    Fixed mail mime-encoded headers

---
+++ profiles/virdini/vbase/src/Plugin/Mail/VBaseMail.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\vbase\Plugin\Mail\VBaseMail.
+ */
+namespace Drupal\vbase\Plugin\Mail;
+
+use Drupal\vbase\VBaseUnicode;
+use Drupal\Component\Utility\Unicode;
+use Drupal\Core\Mail\Plugin\Mail\PhpMail;
+use Drupal\Core\Site\Settings;
+
+/**
+ * Defines the plugin Mail.
+ *
+ * @Mail(
+ *   id = "vbase_mail",
+ *   label = @Translation("VBase mailer"),
+ *   description = @Translation("Sends the message, using PHP's native mail() function.")
+ * )
+ */
+class VBaseMail extends PHPMail {
+  
+  public function mail(array $message) {
+    // If 'Return-Path' isn't already set in php.ini, we pass it separately
+    // as an additional parameter instead of in the header.
+    if (isset($message['headers']['Return-Path'])) {
+      $return_path_set = strpos(ini_get('sendmail_path'), ' -f');
+      if (!$return_path_set) {
+        $message['Return-Path'] = $message['headers']['Return-Path'];
+        unset($message['headers']['Return-Path']);
+      }
+    }
+    $mimeheaders = array();
+    foreach ($message['headers'] as $name => $value) {
+      $mimeheaders[] = $name . ': ' . VBaseUnicode::mimeHeaderEncode($value);
+    }
+    $line_endings = Settings::get('mail_line_endings', PHP_EOL);
+    // Prepare mail commands.
+    $mail_subject = Unicode::mimeHeaderEncode($message['subject']);
+    // Note: email uses CRLF for line-endings. PHP's API requires LF
+    // on Unix and CRLF on Windows. Drupal automatically guesses the
+    // line-ending format appropriate for your system. If you need to
+    // override this, adjust $settings['mail_line_endings'] in settings.php.
+    $mail_body = preg_replace('@\r?\n@', $line_endings, $message['body']);
+    // For headers, PHP's API suggests that we use CRLF normally,
+    // but some MTAs incorrectly replace LF with CRLF. See #234403.
+    $mail_headers = join("\n", $mimeheaders);
+
+    $request = \Drupal::request();
+
+    // We suppress warnings and notices from mail() because of issues on some
+    // hosts. The return value of this method will still indicate whether mail
+    // was sent successfully.
+    if (!$request->server->has('WINDIR') && strpos($request->server->get('SERVER_SOFTWARE'), 'Win32') === FALSE) {
+      // On most non-Windows systems, the "-f" option to the sendmail command
+      // is used to set the Return-Path. There is no space between -f and
+      // the value of the return path.
+      $additional_headers = isset($message['Return-Path']) ? '-f' . $message['Return-Path'] : '';
+      $mail_result = @mail(
+        $message['to'],
+        $mail_subject,
+        $mail_body,
+        $mail_headers,
+        $additional_headers
+      );
+    }
+    else {
+      // On Windows, PHP will use the value of sendmail_from for the
+      // Return-Path header.
+      $old_from = ini_get('sendmail_from');
+      ini_set('sendmail_from', $message['Return-Path']);
+      $mail_result = @mail(
+        $message['to'],
+        $mail_subject,
+        $mail_body,
+        $mail_headers
+      );
+      ini_set('sendmail_from', $old_from);
+    }
+
+    return $mail_result;
+  }
+}
---
+++ profiles/virdini/vbase/src/VBaseUnicode.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\vbase\VBaseUnicode.
+ */
+
+namespace Drupal\vbase;
+
+use Drupal\Component\Utility\Unicode;
+
+class VBaseUnicode extends Unicode {
+  public static function mimeHeaderEncode($string) {
+    if (preg_match('/[^\x20-\x7E]/', $string)) {
+      $chunk_size = 47; // floor((75 - strlen("=?UTF-8?B??=")) * 0.75);
+
+      $suffix = FALSE;
+      if (preg_match('/ <([^>]*)>$/', $string, $match) &&
+          \Drupal::service('email.validator')->isValid($match[1])) {
+        $suffix = $match[0];
+        $suffix_len = self::strlen($suffix);
+        $string = self::truncate($string, self::strlen($string) - $suffix_len);
+      }
+
+      $len = strlen($string);
+      $output = '';
+      while ($len > 0) {
+        $chunk = static::truncateBytes($string, $chunk_size);
+        $output .= ' =?UTF-8?B?' . base64_encode($chunk) . "?=\n";
+        $c = strlen($chunk);
+        $string = substr($string, $c);
+        $len -= $c;
+      }
+      
+      if ($suffix && $suffix_len + $c > $chunk_size) {
+        $suffix = "\n" . $suffix;
+      }
+      $output = trim($output);
+      $result = $suffix ? $output . $suffix : $output;
+      return $result;
+    }
+    return $string;
+  }
+}
