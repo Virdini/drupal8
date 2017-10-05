21fa21fe2a05b31ba50b35e125a3e409fd3ff7d4
Alexander Tsybenko <alexander@tsybenko.info>
2017-10-05 18:41:58 +0300

    Updated development & default configs

--- sites/default/dev.settings.php
+++ sites/default/dev.settings.php
@@ -5,12 +5,12 @@
  * Local development override configuration feature.
  *
  * To activate this feature, copy and rename it such that its path plus
- * filename is 'sites/default/settings.local.php'. Then, go to the bottom of
+ * filename is 'sites/default/dev.settings.php'. Then, go to the bottom of
  * 'sites/default/settings.php' and uncomment the commented lines that mention
- * 'settings.local.php'.
+ * 'dev.settings.php'.
  *
  * If you are using a site name in the path, such as 'sites/example.com', copy
- * this file to 'sites/example.com/settings.local.php', and uncomment the lines
+ * this file to 'sites/example.com/dev.settings.php', and uncomment the lines
  * at the bottom of 'sites/example.com/settings.php'.
  */
 
@@ -53,11 +53,6 @@ $config['system.performance']['css']['preprocess'] = FALSE;
 $config['system.performance']['js']['preprocess'] = FALSE;
 
 /**
-  * Disable AdvAgg CSS and JS aggregation.
-  */
-$config['advagg.settings']['enabled'] = FALSE;
-
-/**
  * Disable the render cache (this includes the page cache).
  *
  * Note: you should test with the render cache enabled, to ensure the correct
@@ -69,7 +64,15 @@ $config['advagg.settings']['enabled'] = FALSE;
  *
  * Do not use this setting until after the site is installed.
  */
-# $settings['cache']['bins']['render'] = 'cache.backend.null';
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+
+/**
+ * Disable caching for migrations.
+ *
+ * Uncomment the code below to only store migrations in memory and not in the
+ * database. This makes it easier to develop custom migrations.
+ */
+$settings['cache']['bins']['discovery_migration'] = 'cache.backend.memory';
 
 /**
  * Disable Dynamic Page Cache.
@@ -78,7 +81,7 @@ $config['advagg.settings']['enabled'] = FALSE;
  * cacheability metadata is present (and hence the expected behavior). However,
  * in the early stages of development, you may want to disable it.
  */
-# $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
 
 /**
  * Allow test modules and themes to be installed.
@@ -87,7 +90,7 @@ $config['advagg.settings']['enabled'] = FALSE;
  * During development it can be useful to install test extensions for debugging
  * purposes.
  */
-$settings['extension_discovery_scan_tests'] = TRUE;
+# $settings['extension_discovery_scan_tests'] = TRUE;
 
 /**
  * Enable access to rebuild.php.
@@ -99,6 +102,19 @@ $settings['extension_discovery_scan_tests'] = TRUE;
  */
 $settings['rebuild_access'] = TRUE;
 
-$settings['cache']['bins']['render'] = 'cache.backend.null';
-$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+/**
+ * Skip file system permissions hardening.
+ *
+ * The system module will periodically check the permissions of your site's
+ * site directory to ensure that it is not writable by the website user. For
+ * sites that are managed with a version control system, this can cause problems
+ * when files in that directory such as settings.php are updated, because the
+ * user pulling in the changes won't have permissions to modify files in the
+ * directory.
+ */
+$settings['skip_permissions_hardening'] = TRUE;
 
+/**
+  * Disable AdvAgg CSS and JS aggregation.
+  */
+$config['advagg.settings']['enabled'] = FALSE;
