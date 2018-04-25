761e6a6f7633de3d77185b4078ed0ffc39a6a6cd
Alexander Tsybenko <alexander@tsybenko.info>
2018-04-25 17:34:15 +0300

    Hotfix Secure text filter

--- profiles/virdini/vbase/src/Plugin/Filter/Secure.php
+++ profiles/virdini/vbase/src/Plugin/Filter/Secure.php
@@ -10,7 +10,7 @@ use Drupal\Component\Utility\Html;
  * @Filter(
  *   id = "vbase_secure",
  *   title = @Translation("Virdini Secure"),
- *   type = Drupal\filter\Plugin\FilterInterface::TYPE_HTML_RESTRICTOR,
+ *   type = Drupal\filter\Plugin\FilterInterface::TYPE_TRANSFORM_IRREVERSIBLE,
  *   weight = -100
  * )
  */
@@ -20,28 +20,19 @@ class Secure extends FilterBase {
    * {@inheritdoc}
    */
   public function process($text, $langcode) {
-    $tags = $this->getHTMLRestrictions();
-    $text = preg_replace('#<(' . implode( '|', $tags['forbidden_tags']) . ')(?:[^>]+)?>.*?</\1>#s', '', $text);
+    $tags = [
+      'script',
+      'style',
+      'embed',
+      'object',
+    ];
+    $text = preg_replace('#<(' . implode( '|', $tags) . ')(?:[^>]+)?>.*?</\1>#s', '', $text);
     $dom = Html::load($text);
     foreach ($dom->getElementsByTagName('link') as $href) {
       $href->parentNode->removeChild($href);
     }
     $text = Html::serialize($dom);
     return new FilterProcessResult($text);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getHTMLRestrictions() {
-    return [
-      'forbidden_tags' => [
-        'script',
-        'style',
-        'embed',
-        'object',
-      ],
-    ];
   }
 
 }
