8c4a094d973335f5ef9cfa5a71f04be6424c0265
Alexander Tsybenko <alexander@tsybenko.info>
2020-12-21 15:08:15 +0200

    Updated Drupal core to 8.9.11

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit7320d926879abcbedcc51a3c63fd2d6a
+class ComposerStaticInit9de9436f91c599173609d35a420479fb
 {
     public static $files = array (
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
@@ -403,11 +403,11 @@ class ComposerStaticInit7320d926879abcbedcc51a3c63fd2d6a
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit7320d926879abcbedcc51a3c63fd2d6a::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit7320d926879abcbedcc51a3c63fd2d6a::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit7320d926879abcbedcc51a3c63fd2d6a::$prefixesPsr0;
-            $loader->fallbackDirsPsr0 = ComposerStaticInit7320d926879abcbedcc51a3c63fd2d6a::$fallbackDirsPsr0;
-            $loader->classMap = ComposerStaticInit7320d926879abcbedcc51a3c63fd2d6a::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit9de9436f91c599173609d35a420479fb::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit9de9436f91c599173609d35a420479fb::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit9de9436f91c599173609d35a420479fb::$prefixesPsr0;
+            $loader->fallbackDirsPsr0 = ComposerStaticInit9de9436f91c599173609d35a420479fb::$fallbackDirsPsr0;
+            $loader->classMap = ComposerStaticInit9de9436f91c599173609d35a420479fb::$classMap;
 
         }, null, ClassLoader::class);
     }
