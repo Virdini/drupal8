1d1f14e70b8991b4b10affc0b9a0f1c6a13b6b7c
Alexander Tsybenko <alexander@tsybenko.info>
2021-01-12 13:25:06 +0200

    Updated Drupal core to 8.8.12

--- vendor/drupal/core-vendor-hardening/Config.php
+++ vendor/drupal/core-vendor-hardening/Config.php
@@ -96,6 +96,9 @@ class Config {
     'symfony-cmf/routing' => ['Test', 'Tests'],
     'theseer/tokenizer' => ['tests'],
     'twig/twig' => ['doc', 'ext', 'test', 'tests'],
+    'zendframework/zend-escaper' => ['doc'],
+    'zendframework/zend-feed' => ['doc'],
+    'zendframework/zend-stdlib' => ['doc'],
   ];
 
   /**
